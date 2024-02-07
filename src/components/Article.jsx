b37286861f6df6c5a698b812c12121f8ab2fd1d1
Mohammed Sajjadul <mohammedsajjadul@gmail.com>
2024-02-07 14:46:57 +0000

    added functionality for buttons to upVote and downVote and also refactored the SingleArticlePage to make it responsive

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -6,21 +6,30 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Button } from "@mui/material";
 
 export default function Article({ article }) {
-  const [showDetails, setShowDetails] = useState(false);
-  const toggleDetails = () => {
-    setShowDetails(!showDetails);
+  const [votes, setVotes] = useState(0);
+  useEffect(() => {
+    localStorage.setItem(
+      "article_${article.article_id}_votes",
+      votes.toString()
+    );
+  }, [article.article_id, votes]);
+  const handleUpvote = () => {
+    setVotes((prevVotes) => Math.max(prevVotes + 1, 0));
+  };
+  const handleDownvote = () => {
+    setVotes((prevVotes) => Math.max(prevVotes - 1, 0));
   };
   if (!article) {
-    return null;
+    return "No article found";
   }
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
       <Card sx={{ maxWidth: 345, margin: 2 }}>
-        <CardActionArea onClick={toggleDetails}>
+        <CardActionArea>
           <CardMedia
             component="img"
             height="140"
@@ -43,20 +52,29 @@ export default function Article({ article }) {
               <br />
               Topic: {article.topic}
               <br />
-              Read More:
+              Votes: {votes}
               <br />
+              Comment Count: {article.comment_count};
             </Typography>
           </CardContent>
         </CardActionArea>
-        {showDetails && (
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              Comment Count: {article.comment_count};
-              <br />
-              Votes: {article.votes}
-            </Typography>
-          </CardContent>
-        )}
+        <CardContent>
+          <Button
+            onClick={handleUpvote}
+            variant="contained"
+            color="primary"
+            sx={{ mr: 2 }}
+          >
+            Up Vote
+          </Button>
+          <Button
+            onClick={handleDownvote}
+            variant="contained"
+            color="secondary"
+          >
+            Down Vote
+          </Button>
+        </CardContent>
       </Card>
     </Grid>
   );
